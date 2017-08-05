3ddd6190467e8cdf28dc16789dd8fb7d076b921e
Connor Bär <connor.baer@sumup.com>
2017-08-05 23:09:56 +0200

    Listen to message events

--- routes/api/events.js
+++ routes/api/events.js
@@ -8,14 +8,40 @@ const router = express.Router();
 
 /* Post events challenge */
 router.post('/', (req, res) => {
-  const { token, challenge } = req.body;
-  logger.info(`Challenge: ${challenge}`);
+  const { response_url, event } = req.body;
 
-  if (token === SLACK_VERIFICATION_TOKEN) {
-    res.json({ challenge });
+  if (!event) {
+    res.status(422).end('No event specified');
     return;
   }
-  res.status(401).end('Unauthorized');
+
+  if (event.type === 'url_verification') {
+    const { token, challenge } = req.body;
+    logger.info(`Challenge: ${challenge}`);
+
+    if (token === SLACK_VERIFICATION_TOKEN) {
+      res.json({ challenge });
+      return;
+    }
+    res.status(401).end('Unauthorized');
+  }
+
+  // Best practice to respond with empty 200 status code.
+  res.status(200).end();
+
+  let message = {};
+
+  switch (event.type) {
+    case 'message':
+      message = {
+        response_type: 'in_channel',
+        text: 'Hi there'
+      };
+      respond(response_url, message);
+      break;
+    default:
+      logger.info(event.type);
+  }
 });
 
 module.exports = router;
