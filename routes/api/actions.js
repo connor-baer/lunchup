d606e1c587924777859ef81c6dd0a2cb6348161e
Connor Bär <connor.baer@sumup.com>
2017-08-06 20:27:53 +0200

    Improve snoozing

--- routes/api/actions.js
+++ routes/api/actions.js
@@ -32,25 +32,26 @@ router.post('/', (req, res) => {
   logger.info(`Action: ${action.name}`);
 
   switch (action.name) {
-    case 'opt_in':
-      addUser(team.id, user);
-      message = {
-        response_type: 'ephermal',
-        text: `🎉 Awesome! Happy to have you on board.`,
-        replace_original: true
-      };
-      break;
-    case 'opt_out':
-      removeUser(team.id, user);
-      message = {
-        response_type: 'ephermal',
-        text: `😔 Alright. Should you change your mind in the future, send me a message @lunchup.`,
-        replace_original: true
-      };
+    case 'join':
+      if (action.value === 'true') {
+        addUser(team.id, user);
+        message = {
+          response_type: 'ephermal',
+          text: `🎉 Awesome! Happy to have you on board.`,
+          replace_original: true
+        };
+      } else {
+        removeUser(team.id, user);
+        message = {
+          response_type: 'ephermal',
+          text: `😔 Alright. Should you change your mind in the future, send me a message @lunchup.`,
+          replace_original: true
+        };
+      }
       break;
     case 'snooze':
-      // TODO: How long?
-      snoozeUser(team.id, user);
+      const timestamp = new Date(+new Date + (1000 * 60 * 60 * 24 * 7 * Number(action.value));
+      snoozeUser(team.id, user, timestamp);
       message = {
         response_type: 'ephermal',
         text: `🗓 Alright! I'll include you again in a week.`,
